582b75976862b7a0974ec312ea7d1b728ff24f4d
Dreaserous <dreaserous2.0@gmail.com>
2024-12-18 11:03:52 +0530

    README Commit #2

---
+++ README.md
@@ -0,0 +1,12 @@
+# **3D Asset Automation Tool**
+
+## **Instructions**
+
+### **1. Close All Chrome Instances**
+Before starting the script, ensure all running instances of Chrome are closed. You can do this manually.
+
+### **2. Run the Script**
+To start the automation process, run the following command:
+```bash
+python main.py
+```
--- setup.py
+++ setup.py
@@ -1,6 +1,26 @@
 import os
-import sys
 from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+class PostInstallCommand(install):
+    """Custom post-install command to update the .env file."""
+
+    def run(self):
+        # Run the standard install process
+        install.run(self)
+        
+        # Add or modify the .env file
+        env_file_path = os.path.join(os.getcwd(), ".env")
+        user = os.getenv("USERNAME") or os.getenv("USER")
+        
+        # Update .env file
+        if user:
+            user_data_dir = f"C:/Users/{user}/AppData/Local/Google/Chrome/User Data"
+            with open(env_file_path, "a") as env_file:
+                env_file.write(f"\nUSER_DATA_DIR={user_data_dir}\n")
+            print(f"Updated .env with USER_DATA_DIR: {user_data_dir}")
+        else:
+            print("Could not determine the user. .env file not updated.")
 
 setup(
     name="Fab Automation",
@@ -10,13 +30,16 @@ setup(
     description="A package for auto-uploading, editing and deleting models on the fab website.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/DreaserousProductions/your-repo-name",
+    url="https://github.com/DreaserousProductions/FabAutomation",
     packages=find_packages(),
     install_requires=[
         "selenium",
         "pyqt5",
         "python-dotenv"
     ],
+    cmdclass={
+        'install': PostInstallCommand,
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -24,6 +47,3 @@ setup(
     ],
     python_requires=">=3.7",
 )
-
-
-USER_DATA_DIR = f"C:/Users/{user}/AppData/Local/Google/Chrome/User Data"
